61cde587cffff2ac3deeee98f85da221fceba691
mikelear <mike.lear@cityindex.com>
2019-04-09 08:56:59 +0100

    Initial commit

---
+++ src/karma.conf.js
@@ -0,0 +1,43 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+const isDocker = require('is-docker')();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage/temp-then-delete'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['MyHeadlessChrome'],
+    customLaunchers: {
+       MyHeadlessChrome : {
+         base: 'ChromeHeadless',
+          // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+          // more permissions than Docker allows by default)
+          flags: isDocker ? ['--no-sandbox'] : []
+     }
+    },
+
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
